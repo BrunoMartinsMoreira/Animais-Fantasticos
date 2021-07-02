238140e808210e303e65be5f584725cc622c417a
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-07-02 18:38:55 -0300

    adicionando accordion navigation na sessao faq

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-const tabMenu = document.querySelectorAll('.tabmenu li');
-const tabContent = document.querySelectorAll('.tabcontent section');
+function tabNav() {
+   const tabMenu = document.querySelectorAll('.tabmenu li');
+   const tabContent = document.querySelectorAll('.tabcontent section');
 
-if (tabMenu.length && tabContent.length) {
-   tabContent[0].classList.add('active')
-   //Recebe index como parâmetro para ativar a tab. 
-   //Sempre que ativar, remove a classe active
-   // de todos os outros elementos.
-   function activeTab(index) {
-      tabContent.forEach((content) => {
-         content.classList.remove('active');
+   if (tabMenu.length && tabContent.length) {
+      tabContent[0].classList.add('active')
+      //Recebe index como parâmetro para ativar a tab. 
+      //Sempre que ativar, remove a classe active
+      // de todos os outros elementos.
+      function activeTab(index) {
+         tabContent.forEach((content) => {
+            content.classList.remove('active');
+         });
+         tabContent[index].classList.add('active');
+      }
+
+      //adicionar evento -click- em cada imagem
+      tabMenu.forEach((itemMenu, index) => {
+         itemMenu.addEventListener('click', () => {
+            activeTab(index);
+         });
       });
-      tabContent[index].classList.add('active');
    }
-
-   //adicionar evento -click- em cada imagem
-   tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-         activeTab(index);
-      });
-   });
 }
+tabNav();
+
+function accordionNavigation() {
+   const accordionList = document.querySelectorAll('.accordion dt');
+   const activeClass = 'active';
+
+   if (accordionList.length) {
+      accordionList[0].classList.add(activeClass);
+      accordionList[0].nextElementSibling.classList.add(activeClass);
+
+      function activeAccordion() {
+         this.classList.toggle(activeClass);
+         this.nextElementSibling.classList.toggle(activeClass);
+      }
+
+      accordionList.forEach((item) => {
+         item.addEventListener('click', activeAccordion);
+      })
+   }
+}
+accordionNavigation();
