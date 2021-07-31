b22e7d7a0e08007c7e814d30bb3840098b35d968
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-07-30 22:49:48 -0300

    finalizando modal

---
+++ js/module/modal.js
@@ -0,0 +1,28 @@
+export default function initModal() {
+   const openBtn = document.querySelector('[data-modal="abrir"]');
+   const closeBtn = document.querySelector('[data-modal="fechar"]');
+   const modalContainer = document.querySelector('[data-modal="container"]');
+
+   if (openBtn && closeBtn && modalContainer) {
+      function openModal(event) {
+         event.preventDefault()
+         modalContainer.classList.add('ativo')
+      }
+
+      function closeModal(event) {
+         event.preventDefault()
+         modalContainer.classList.remove('ativo')
+      }
+
+      function clickContainerToCloseModal(event) {
+         if (event.target === this) {
+            closeModal(event)
+         }
+
+      }
+
+      openBtn.addEventListener('click', openModal);
+      closeBtn.addEventListener('click', closeModal);
+      modalContainer.addEventListener('click', clickContainerToCloseModal);
+   }
+}
--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ import softScroll from "./module/softScroll.js";
 import initAnimatedScroll from "./module/scrollAnimation.js";
 import accordionNavigation from "./module/accordionNav.js";
 import tabNav from "./module/tabNav.js";
+import initModal from "./module/modal.js"
 
 softScroll();
 initAnimatedScroll();
 accordionNavigation();
 tabNav();
+initModal();
 
 
 
