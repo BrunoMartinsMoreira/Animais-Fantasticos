1d914ccdfb541e2d6bd11e91d4e3def9696593b3
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-08-01 00:41:39 -0300

    finalizando dropdown menu e criando modulo outside click

--- js/module/dropDownMenu.js
+++ js/module/dropDownMenu.js
@@ -1,13 +1,18 @@
+import outsideClick from "./outsideClick.js";
+
 export default function initDropDownMenu() {
    const dropDownMenus = document.querySelectorAll('[data-dropdown]');
    dropDownMenus.forEach(menu => {
       ['touchstart', 'click'].forEach(userEvent => {
          menu.addEventListener(userEvent, handleClick)
       })
-   })
+   });
 
    function handleClick(event) {
       event.preventDefault()
-      this.classList.toggle('active')
-   }
+      this.classList.add('active')
+      outsideClick(this, ['touchstart', 'click'], () => {
+         this.classList.remove('active')
+      })
+   };
 }
