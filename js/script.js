fca5a33dda583dbf0106fc947724a97115d7d280
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-08-03 22:00:41 -0300

    finalizando menu mobile com js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import tabNav from "./module/tabNav.js";
 import initModal from "./module/modal.js";
 import initTooltip from "./module/toolTip.js";
 import initDropDownMenu from './module/dropDownMenu.js';
+import initMenuMobile from './module/menuMobile.js'
 
 softScroll();
 initAnimatedScroll();
@@ -13,6 +14,7 @@ tabNav();
 initModal();
 initTooltip();
 initDropDownMenu();
+initMenuMobile();
 
 
 
