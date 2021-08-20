bde146b10d270e0844ac16464cf02295a74e0977
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-08-19 21:31:34 -0300

    consumindo api do bitcoin

--- js/module/fetchBtc.js
+++ js/module/fetchBtc.js
@@ -1,3 +1,19 @@
 export default function initFetchBtc() {
+   const btcContainer = document.querySelector('.btc-preco');
+   if (btcContainer) {
+      function getBtcInfo() {
+         try {
+            fetch('https://blockchain.info/ticker')
+               .then(response => response.json())
+               .then(btcInfosJson => {
+                  console.log(btcInfosJson.BRL.buy)
+                  btcContainer.innerText = (1000 / btcInfosJson.BRL.buy).toFixed(5)
+               })
+         } catch (err) {
+            console.log(err)
+         };
+      }
+      getBtcInfo()
+   }
 
 }
